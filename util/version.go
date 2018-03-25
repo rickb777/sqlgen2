515479307251ba7067f874e03c722655c2a812d0
Rick <1358735+rickb777@users.noreply.github.com>
2018-03-25 13:49:50 +0100

    Altered the versioning scripting so that 'go get' works too.

---
+++ util/version.go
@@ -0,0 +1,7 @@
+// Updated automatically
+package util
+
+const Version    = "(unavailable)"
+const GitBranch  = "(unavailable)"
+const GitOrigin  = "git@github.com:rickb777/sqlgen2.git"
+const BuildDate  = "(unavailable)"
--- version.sh
+++ version.sh
@@ -2,8 +2,9 @@
 
 VFILE=util/version.go
 
-echo "// Generated automatically" > $VFILE
+echo "// Updated automatically" > $VFILE
 echo "package util" >> $VFILE
+echo "" >> $VFILE
 echo "const Version    = \"$(git describe --tags --dirty 2>/dev/null)\"" >> $VFILE
 #echo "const CommitHash = \"$(git rev-parse --short HEAD 2>/dev/null)\"" >> $VFILE
 echo "const GitBranch  = \"$(git symbolic-ref -q --short HEAD 2>/dev/null)\"" >> $VFILE
