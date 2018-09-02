e963d066450c4aaefd1acfd811da4dbe4894fcff
Rick <1358735+rickb777@users.noreply.github.com>
2018-09-02 22:02:56 +0100

    this package is now a go module

--- go-get.sh
+++ go-get.sh
@@ -2,11 +2,6 @@
 # Speeds up build time by skipping unnecessary repeated scanning of Github sources.
 # This has not been tested on Windows; it probably works but saves no time.
 
-if [ -z "$GOPATH" ]; then
-  echo "GOPATH must be set. Use only one path in it (i.e. no colons)."
-  exit 1
-fi
-
 function getBinary
 {
   if ! type -p $1 >/dev/null; then
@@ -15,8 +10,7 @@ function getBinary
   fi
 }
 
-getBinary dep         github.com/golang/dep/cmd/dep
 getBinary runtemplate github.com/rickb777/runtemplate
 getBinary goveralls   github.com/mattn/goveralls
 
-dep ensure
+#dep ensure
